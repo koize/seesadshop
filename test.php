38207e2a73b0d972a7febaec2a201bb3e9ae09c7
duckupus <75720972+duckupus@users.noreply.github.com>
2023-07-30 01:46:12 +0800

    added "prizes"

--- test.php
+++ test.php
@@ -8,8 +8,9 @@
 </head>
 <script>
     class AABB {
-        constructor(center, halfx, halfy) {
-            this.center = center;
+        constructor(centerX, centerY, halfx, halfy) {
+            this.centerX = centerX;
+            this.centerY = centerY;
             this.halfx = halfx;
             this.halfy = halfy;
         }
@@ -19,13 +20,23 @@
         getHalfY() {
             return this.halfy;
         }
-        getCenter() {
-            return this.center;
+        getCenterX() {
+            return this.centerX;
+        }
+        setCenterX(x) {
+            this.centerX = x;
+        }
+        getCenterY() {
+            return this.centerY;
+        }
+        setCenterY(y) {
+            this.centerY = y;
         }
         colliding(ayaybebe) {
-            var offset = Math.abs(this.center - ayaybebe.getCenter());
-            if (offset > this.halfx + ayaybebe.getHalfX()) return false;
-            if (offset > this.halfy + ayayabebe.getHalfY()) return false;
+            var offsetX = Math.abs(this.centerX - ayaybebe.getCenterX());
+            var offsetY = Math.abs(this.centerY - ayaybebe.getCenterY());
+            if (offsetX > this.halfx + ayaybebe.getHalfX()) return false;
+            if (offsetY > this.halfy + ayaybebe.getHalfY()) return false;
             return true;
         }
     }
@@ -65,7 +76,8 @@
         y: 6,
         //player velocity
         dx: 2,
-        dy: 2
+        dy: 2,
+        AABB: new AABB(0, 0, 32 / 2, 32 / 2)
     }
     const movementX = 2;
     const movementY = 2;
@@ -89,29 +101,54 @@
         if (e.keyCode == ' '.charCodeAt(0)) { //spacebar
 
         } else if (e.keyCode == 'W'.charCodeAt(0)) { //W
-            if(player.dy == movementY) player.dy = 0;
+            if (player.dy == movementY) player.dy = 0;
         } else if (e.keyCode == 'A'.charCodeAt(0)) { //A
-            if(player.dx == -movementX) player.dx = 0;
+            if (player.dx == -movementX) player.dx = 0;
         } else if (e.keyCode == 'D'.charCodeAt(0)) { //D
-            if(player.dx == movementX) player.dx = 0;
+            if (player.dx == movementX) player.dx = 0;
         } else if (e.keyCode == 'S'.charCodeAt(0)) { //S
-            if(player.dy == -movementY) player.dy = 0;
+            if (player.dy == -movementY) player.dy = 0;
         }
     });
+
+    function generateLoot() {
+        //generate loot
+        return new AABB(Math.floor(Math.random() * (canvas.width - 16/2)), Math.floor(Math.random() * (canvas.height - 16/2)), 16 / 2, 16 / 2);
+        return lootAABB;
+    }
+    var loot = generateLoot();
+    let hscore = 0;
+    let score = 0;
+
     function loop() {
         //clear the screen
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        //draw the player
-        ctx.fillStyle = "#008080";
-        ctx.fillRect(player.x, player.y, 32, 32);
+        if (player.AABB.colliding(loot)) {
+            while (player.AABB.colliding(loot)) {
+                loot = generateLoot();
+            }
+            if (hscore < ++score) {
+                hscore = score;
+                document.getElementById("high_score").innerHTML = hscore;
+            }
+            document.getElementById("score").innerHTML = score;
+        }
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(loot.getCenterX(), loot.getCenterY(), 16, 16);
         //update the player
         player.x += player.dx;
         player.y -= player.dy; //subtract because y is inverted
         //check for collisions with map
-        if(player.x < 0) player.x = 0;
-        if(player.x > canvas.width - 32) player.x = canvas.width - 32;
-        if(player.y < 0) player.y = 0;
-        if(player.y > canvas.height - 32) player.y = canvas.height - 32;
+        if (player.x < 0) player.x = 0;
+        if (player.x > canvas.width - 32) player.x = canvas.width - 32;
+        if (player.y < 0) player.y = 0;
+        if (player.y > canvas.height - 32) player.y = canvas.height - 32;
+
+        player.AABB.setCenterX(player.x);
+        player.AABB.setCenterY(player.y);
+        //draw the player
+        ctx.fillStyle = "#008080";
+        ctx.fillRect(player.x, player.y, 32, 32);
 
         //request another frame
         requestAnimationFrame(loop);
