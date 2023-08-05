a3090d275631041f3845334cdf709001690bcc58
koize <justinweng6@gmail.com>
2023-08-05 21:59:10 +0800

    home screen kinda done (ui only)

---
+++ about.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>seeee saaad shop</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- Material Icons3 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,400,0,0" /> 
+    <script src="promotion.js"></script>
+
+  
+  </head>
+<body>
+        <!--Main Navigation-->
+    <header>
+      <style>
+        /* Carousel styling */
+        #introCarousel,
+        .carousel-inner,
+        .carousel-item,
+        .carousel-item.active {
+          height: 100vh;
+        }
+
+        .carousel-item:nth-child(1) {
+          background-image: 'img/carousel_pmnt.jpg';
+          background-repeat: no-repeat;
+          background-size: cover;
+          background-position: center center;
+        }
+    
+
+        /* Height for devices larger than 576px */
+        @media (min-width: 992px) {
+          #introCarousel {
+            margin-top: -58.59px;
+          }
+          #introCarousel,
+          .carousel-inner,
+          .carousel-item,
+          .carousel-item.active {
+            height: 47vh;
+          }
+        }
+
+        .navbar .nav-link {
+          color: #fff !important;
+        }
+      </style>
+
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-dark d-none d-lg-block bg-info bg-gradient" style="z-index: 2000;" height="150%">
+        <div class="container-fluid">
+          <!-- Navbar brand -->
+          <a class="navbar-brand nav-link" target="_blank" href="index.php">
+          <img src="img/logo.png"  alt="a" width= "200" height= "55">
+          </a>
+          <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01"
+            aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarExample01">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow"
+                  target="_blank">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="game.php" target="_blank">Rewards</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://mdbootstrap.com/docs/standard/" target="_blank">Support</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php" target="_blank">About us</a>
+              </li> 
+            </ul>
+
+            <ul class="navbar-nav d-flex flex-row">
+              <!-- Icons -->
+              <li class="nav-item me-3 me-lg-0">
+              <form class="d-flex input-group w-auto">
+      <input
+        type="search"
+        class="form-control rounded"
+        placeholder="Search all products"
+        aria-label="Search"
+        aria-describedby="search-addon"
+      />
+      <span class="input-group-text border-0" id="search-addon">
+        <i class="fas fa-search"></i>
+      </span>
+    </form>
+              </li>
+             
+              <li class="nav-item me-3 me-lg-0">
+                <a class="nav-link" href="https://twitter.com/MDBootstrap" rel="nofollow" target="_blank">
+                <i class="fas fa-cart-arrow-down"></i>
+                </a>
+              </li>
+              <li class="nav-item me-3 me-lg-0">
+                <a class="nav-link" href="https://github.com/mdbootstrap/mdb-ui-kit" rel="nofollow" target="_blank">
+                <i class="fas fa-user"></i>
+                </a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- Navbar -->
+    </header>
+    <!--Main Navigation-->
+
+    <!--Main layout-->
+    <main class="mt-5">
+      <div class="container">
+      
+         <!--Section: Content-->
+         <section>
+          <div class="row">
+            
+            <div class="col-md-6 gx-5 mb-4">
+              <h4><strong>About us</strong></h4>
+              <p class="text-muted">
+              Seesad is an online shop dedicated to providing high-quality face wash products from Kao and Gatsby. We believe that everyone deserves to have clear, healthy skin, and we are committed to providing our customers with the best possible products to help them achieve their skin care goals.
+              </p>
+              <p><strong>Why Kao (Biore) and Gatsby?</strong></p>
+              <p class="text-muted">
+              Kao (Biore) and Gatsby are two of the leading brands in the face wash industry, and we are proud to offer a wide variety of their products on our website. Our products are made with a variety of natural ingredients, and they are gentle on the skin.
+
+              </p>
+            </div>
+            <div class="col-md-6 gx-5 mb-4">
+              <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+                <img src="img/about_page_kao1.jpg" class="img-fluid" />
+                <img src="img/about_page_gatsby1.png" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+          </div>
+        </section>
+        <!--Section: Content-->
+        <hr class="my-5" />
+         <!--Section: Content-->
+         <section>
+          <div class="row">
+            <div class="col-md-6 gx-5 mb-4">
+              <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+                <img src="img/logo.png" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-6 gx-5 mb-4">
+              <h4><strong>Why choose us?</strong></h4>
+              <p class="text-muted">
+              We offer a variety of different face wash products to suit different skin types. Whether you have oily, dry, or sensitive skin, we have a face wash that is right for you.
+
+In addition to our high-quality products, we also offer excellent customer service. We are always available to answer your questions and help you find the right product for your skin.
+
+We believe that Seesad is the best place to buy face wash online. We offer a wide variety of products from Kao and Gatsby, excellent customer service, and competitive prices. We are confident that you will be happy with your purchase from Seesad.
+
+</p>
+<p><strong>Thank you for choosing Seesad!              
+</strong></p>
+             
+            </div>
+          </div>
+        </section>
+        <!--Section: Content-->
+        <hr class="my-5" />
+        <!--Section: Content-->
+         <section class="mb-5">
+          <h4 class="mb-5 text-center"><strong>Find us!</strong></h4>
+          <div class="col">
+      <div class="row align-items-center justify-content-around">
+        <div class="col-md-6 gx-5 mb-4">
+        <h4>
+            <i class="fas fa-store"></i>            
+            <span class="m-1"></span>
+              Seesad @ Singapore Polytechnic
+            </h4>
+
+            <h4>
+            <i class="fas fa-location-arrow"></i>            
+            <span class="m-1"></span>
+              535 Clementi Rd, Singapore 599489, #T18A307
+            </h4>
+
+            <h4>
+            <i class="fas fa-at"></i>              
+            <span class="m-1"></span>
+              <a href="mailto:sp_aviation@ichat.sp.edu.sg">sp_aviation@ichat.sp.edu.sg</a>
+            </h4>
+
+            <h4>
+            <i class="fas fa-phone"></i>              
+            <span class="m-1"></span>
+              +65 8123 0192
+            </h4>
+
+            <h4>
+            <i class="far fa-clock"></i>              
+            <span class="m-1"></span>
+              Monday to Monday, 9AM to 10AM
+            </h4>
+        </div>
+
+        <br>
+
+        <div class="col text-align-center">
+          <div class="m-5">
+            <div class="iframe-rwd">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.103504695046!2d103.7775495!3d1.3097757!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1b0a73c20cbd%3A0x2e1991d3696d3011!2sSingapore%20Poly!5e0!3m2!1sen!2ssg!4v1691235318368!5m2!1sen!2ssg" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br>
+              <small>
+                <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3708.281749908684!2d103.77215791453851!3d1.332103599028431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da107d8eb4e359%3A0x75d2e7ffdeeb0c43!2sNgee%20Ann%20Polytechnic!5e1!3m2!1sen!2sus!4v1675999527895!5m2!1sen!2sus"
+                  style="color:#0000FF;text-align:left">View Larger Map</a>
+              </small>
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+          
+      
+      </div>
+      </div>
+      
+    </main>
+    <!--Main layout-->
+  <footer class="bg-secondary text-white">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase">about seesad</h5>
+
+          <p>
+          Seesad is an online shop dedicated to providing high-quality, natural face wash products that are gentle on the skin. We believe that everyone deserves to have clear, healthy skin, and we are committed to providing our customers with the best possible products to help them achieve their skin care goals. For more details, go to our <a href="about.php">About Us</a> page.
+          </p>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">the poor developers</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">hackerman gab</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">slave 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">slave 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">slave 3</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0">Quick shortcuts</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-white">View all products</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Free vouchers</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Support</a>
+            </li>
+            <li>
+              <a href="about.php" class="text-white">About us</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+      <section class="mb-1">
+          <!-- Facebook -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+  
+          <!-- Twitter -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+  
+          <!-- Google -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitch"></i></a>
+  
+          <!-- Instagram -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+      </div>
+      <!-- Section: Social media -->
+  
+      <!-- Section: Form -->
+      <div class="container">
+      <section class="">
+        <form action="">
+          <!--Grid row-->
+          <div class="row d-flex justify-content-center">
+            <!--Grid column-->
+            <div class="col-auto">
+              <p class="pt-2">
+                <strong>Sign up for updates!</strong>
+              </p>
+            </div>
+            <!--Grid column-->
+  
+            <!--Grid column-->
+            <div class="col-md-5 col-12">
+              <!-- Email input -->
+              <div class="form-outline form-light mb-4 ">
+                <input type="email" id="newsEmail" class="form-control" style="color: white" />
+                <label class="form-label" for="newsEmail" style="color: white">Email address</label>
+              </div>
+            </div>
+            <!--Grid column-->
+  
+            <!--Grid column-->
+            <div class="col-auto">
+              <!-- Submit button -->
+              <button id="news" type="button" class="btn btn-outline-light mb-4">
+                Subscribe
+              </button>
+            </div>
+            <!--Grid column-->
+          </div>
+          <!--Grid row-->
+        </form>
+      </section>
+      
+      <!-- Section: Form -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
+    <!--Footer-->
+
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
+
