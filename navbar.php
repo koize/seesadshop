c1f7ddedbcec3c0a44f0854787b695609fe2e9bd
koize <justinweng6@gmail.com>
2023-08-08 14:44:12 +0800

    fix nav link

--- navbar.php
+++ navbar.php
@@ -48,7 +48,7 @@
               </ul>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="game.php">Rewards</a>
+              <a class="nav-link" href="rewards.php">Rewards</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="supportPage.php">Support</a>
