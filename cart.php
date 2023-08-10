b048a92ebc622dc091e078e4902cb4eb1cc9be9e
Drew <drewcfrog@gmail.com>
2023-08-10 15:42:37 +0800

    chekout btn

--- cart.php
+++ cart.php
@@ -228,10 +228,8 @@
               <!-- Card end -->
             </div>
             <div class="card" style="width: 100%;">
-              <img src="..." class="card-img-top" alt="...">
               <div class="card-body">
-                <h5 class="card-title">Card with stretched link</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">Checkout $23.19</h5>
                 <a href="#" class="btn btn-primary stretched-link">Go somewhere</a>
               </div>
             </div>
